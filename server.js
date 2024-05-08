837bf3ac0621d597b2eb0acf978d59ae6851c634
Sebastian Vega <sebasvega4604@gmail.com>
2024-05-07 20:27:53 -0400

    test

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.status(200).send('Hello World!');
 });
 
-app.listen(3000, () => {
-    console.log("listening on port 3000");
-});
+// app.listen(3000, () => {
+//     console.log("listening on port 3000");
+// });
+
+module.exports = app;
